975c9cbcc311d65ac352d09463346e99596ca5e1
Tyom <artyom.mikayelyan@padcllc.com>
2024-02-26 15:45:56 +0400

    changed base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
 })
-
-
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   return {
-//     plugins: [react()],
-//     server: {
-//       proxy: {
-//         '/api': {
-//           target: env.VITE_API_BASE_URL,
-//           changeOrigin: true,
-//           secure: false,
-//           rewrite: (path) => path.replace(/^\/api/, ''),
-//         },
-//       }
-//     }
-//   }
-// })
