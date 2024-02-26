a843e06229db33188f2d2c7b8a911b06886fcd20
Tyom <artyom.mikayelyan@padcllc.com>
2024-02-26 15:04:43 +0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       target: 
+//     }
+//   }
+// })
+
+
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
+    }
+  }
+})
